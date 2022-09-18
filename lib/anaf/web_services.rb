6cfe646cd41073d8b72d53aee18e22f7f04d6b19
siklodi-mariusz <9211589+siklodi-mariusz@users.noreply.github.com>
2022-09-18 12:39:20 +0300

    First commit

---
+++ lib/anaf/web_services.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "web_services/version"
+
+module Anaf
+  module WebServices
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/anaf/web_services/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Anaf
+  module WebServices
+    VERSION = "0.1.0"
+  end
+end
